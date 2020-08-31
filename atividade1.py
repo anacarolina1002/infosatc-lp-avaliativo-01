db1d633402deb2b1e09327c37a905b5359c094e5
Anacarolina1002 <carolquaglioto@outlook.com>
2020-08-31 14:40:20 -0300

    feat: adicionando exercício 1

---
+++ atividade1.py
@@ -0,0 +1,5 @@
+#Fahrenheit para Celsius
+#C = Celsius e F = Fahrenheit
+F = float(input ("Insira a temperatura em ºF:"))
+C = (5.0 *(F - 32.0) / 9.0 )
+print("A temperatura",(F),"ºC em Celsius é",(C),"ºC")
